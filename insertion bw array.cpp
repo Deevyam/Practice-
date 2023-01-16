9f8e141beede477de46e3dab027690b0d80b7f7f
deevyam <101313416+69NEWBIE69@users.noreply.github.com>
2023-01-16 10:46:43 +0530

    Add files via upload

---
+++ insertion bw array.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int arr[50],n, i, elem, pos, tot,d;
+    cout<<"Enter the Size for Array: ";
+    cin>>tot;
+    cout<<"Enter "<<tot<<" Array Elements: ";
+    for(i=0; i<tot; i++)
+        cin>>arr[i];
+    cout<<"\nEnter Element to Insert: ";
+    cin>>elem;
+    cout<<"At What Position ";
+    cin>>pos;
+    for(i=tot; i>=pos; i--)
+        arr[i] = arr[i-1];
+    arr[i] = elem;
+    tot++;
+    cout<<"\nThe New Array is:\n";
+    for(i=0; i<tot; i++)
+        cout<<arr[i]<<"  ";
+    cout<<endl;
+    cout<<"enter position to be deleted";
+     /*cin>>pos;
+    for(i=tot; i>=pos; i++)
+        arr[i] = arr[i-1];
+    arr[i] = ;
+    tot++;
+    cout<<endl;*/
+    return 0;
+}
+
